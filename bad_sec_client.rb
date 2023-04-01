f8204fca7d7cc6b849b1b678cf44eadff167687b
Ian Lunderskov <ian@lundersaur.us>
2023-04-01 14:37:37 -0500

    Add fetch_user functionality and related checksum as method for reuse

--- bad_sec_client.rb
+++ bad_sec_client.rb
@@ -1,17 +1,32 @@
 require 'uri'
 require 'net/http'
+require 'digest'
 
 class BadSecClient
   BADSEC_URI = "http://0.0.0.0:8888"
 
   def run
-    STDOUT.puts(authenticate)
+    STDOUT.puts("Call to users gives: " + fetch_users)
   end
 
-  def authenticate
+  def get_auth_token
     uri = URI("#{BADSEC_URI}/auth")
     res = Net::HTTP.get_response(uri)
     token = res['badsec-authentication-token']
+  end
+
+  def checksum(auth_token, request_path)
+    Digest::SHA256.hexdigest(auth_token + request_path)
+  end
+
+  def fetch_users
+    token = get_auth_token
+    uri = URI.parse("#{BADSEC_URI}/users")
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request["X-Request-Checksum"] = checksum(token, '/users')
+
+    response = http.request(request).body
   end
 end
 
