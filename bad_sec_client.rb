008e3da17b3fc346f499c443337ae2c440b5bc61
Ian Lunderskov <ian@lundersaur.us>
2023-04-01 15:23:03 -0500

    Add retry logic to both endpoint calls

--- bad_sec_client.rb
+++ bad_sec_client.rb
@@ -7,13 +7,25 @@ class BadSecClient
   BADSEC_URI = "http://0.0.0.0:8888"
 
   def run
-    STDOUT.puts("Call to users gives: " + fetch_users)
+    STDOUT.puts(fetch_users)
   end
 
   def get_auth_token
     uri = URI("#{BADSEC_URI}/auth")
-    res = Net::HTTP.get_response(uri)
-    token = res['badsec-authentication-token']
+    auth_token = nil
+
+    1.upto(3) do
+      response = Net::HTTP.get_response(uri)
+      if response.code == "200"
+        auth_token = response['badsec-authentication-token']
+        break
+      else
+        sleep 1
+      end
+    end
+
+    exit 1 unless auth_token
+    return auth_token
   end
 
   def checksum(auth_token, request_path)
@@ -22,13 +34,24 @@ class BadSecClient
 
   def fetch_users
     token = get_auth_token
+    users_list = nil
     uri = URI.parse("#{BADSEC_URI}/users")
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Get.new(uri.request_uri)
     request["X-Request-Checksum"] = checksum(token, '/users')
 
-    response = http.request(request).body
-    JSON.generate(response.split)
+    1.upto(3) do
+      response = http.request(request)
+      if response.code == "200"
+        users_list = JSON.generate(response.body.split)
+        break
+      else
+        sleep 1
+      end
+    end
+
+    exit 1 unless users_list
+    return users_list
   end
 end
 
